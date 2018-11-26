cc51f98d1c9b4555d7c03a88762c186a911a1be5
gomescleve <gomescleve@gmail.com>
2018-11-26 18:26:08 +0400

    migration added

--- database/migrations/2018_11_09_190256_create_products_table.php
+++ database/migrations/2018_11_09_190256_create_products_table.php
@@ -17,9 +17,21 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('name')->unique();
             $table->string('slug');
-            $table->string('details')->nullable();
-            $table->integer('price');
-            $table->text('description');
+            $table->decimal('weight', 5, 2);
+            $table->decimal('price', 5, 2);
+            $table->decimal('handling_price', 5, 2);
+            $table->decimal('transportation_price', 5, 2);
+            $table->integer('minimum_order');
+            $table->text('description')->nullable();
+            $table->string('brand')->nullable();
+            $table->text('return_policy')->nullable();
+            $table->text('packaging_type')->nullable();
+            $table->string('model')->nullable();
+            $table->string('grade')->nullable();
+            $table->string('filename')->nullable();
+            $table->string('mime')->nullable();
+            $table->string('original_filename')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
