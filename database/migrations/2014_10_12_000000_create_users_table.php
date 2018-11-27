d8f283c023d723cc78f314be788d3db15fe4e00b
gomescleve <gomescleve@gmail.com>
2018-11-27 14:33:18 +0400

    modals added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('last_name');
             $table->date('dob');
             $table->enum('gender', array('m', 'f'));
-            $table->enum('user_type', array('admin', 'normal'));
+            $table->enum('user_type', array('admin', 'normal'))->default('normal');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
